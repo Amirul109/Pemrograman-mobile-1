dd2804e57d693967d18bbb43abf43c99b40f1989
Amirul109 <116171779+Amirul109@users.noreply.github.com>
2022-10-23 07:28:18 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.qrcodescanner_ti21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
