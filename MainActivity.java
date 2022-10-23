7a88ce1a501ec34c99b5a22584d9fa9c2b8b2c41
Amirul109 <116171779+Amirul109@users.noreply.github.com>
2022-10-23 07:38:54 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,68 @@
-package com.example.qrcodescanner_ti21;
+<?xml version="1.0" encoding="utf-8"?>
+    <RelativeLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+        xmlns:app="http://schemas.android.com/apk/res-auto"
+        xmlns:tools="http://schemas.android.com/tools"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:orientation="vertical"
+        android:padding="10dp"
+        tool:context="MainActivity">
 
-import androidx.appcompat.app.AppCompatActivity;
+    <Button
+            android:id="@+id/buttonScan"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="Scan QR Code"
+            android:layout_alignParentBottom="true" />
 
-import android.os.Bundle;
-import android.widget.EditText;
+    <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:orientation="vertical"
+            android:layout_centerVertical="true">
 
-public class MainActivity extends AppCompatActivity {
+            <TextView
+                android:id="@+id/textView"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:text="Nama" />
+            <TextView
+                android:id="@+id/textViewNama"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:text="Amirul Mu'minin"
+                android:textAppearance="@style/TextAppearance.AppCompat.Large" />
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+            <TextView
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:text="Kelas" />
+
+            <TextView
+                android:id="@+id/textViewKelas"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:text="TI.21.C2"
+                android:textAppearance="@style/TextAppearance.AppCompat.Large" />
+
+            <TextView
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:text="NIM" />
+
+            <TextView
+                android:id="@+id/textViewNIM"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+                android:text="312110109"
+                android:textAppearance="@style/TextAppearance.AppCompat.Large"
+
+
+        <ImageView
+            android:id="@+id/imageView"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            tools:srcCompat="@tools:sample/backgrounds/scenic" />
+    </LinearLayout>
+    </RelativeLayout>
